531a469b016da473d914148a41337ec675e9c077
TingjiaZhang <31323496+TingjiaZhang@users.noreply.github.com>
2021-03-23 16:31:18 +0800

    Add files via upload

---
+++ cmpe277-2/Model/NewsData.swift
@@ -0,0 +1,8 @@
+//
+//  NewsData.swift
+//  cmpe277
+//
+//  Created by 张廷嘉 on 3/23/21.
+//
+
+import Foundation
---
+++ cmpe277-2/cmpe277/ViewTwoController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewTwoController.swift
+//  cmpe277
+//
+//  Created by 张廷嘉 on 3/23/21.
+//
+
+import UIKit
+
+class ViewTwoController: UIViewController {
+
+    @IBOutlet weak var TestTxtField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
